49f2133d3345ba3c38440245d55aa0c77b82d98d
faraz <farazyazdamib@gmail.com>
2024-07-24 10:56:30 +0330

    change service yamls

--- shopfront/src/main/java/uk/co/danielbryant/djshopping/shopfront/controllers/HomeController.java
+++ shopfront/src/main/java/uk/co/danielbryant/djshopping/shopfront/controllers/HomeController.java
@@ -3,6 +3,7 @@ package uk.co.danielbryant.djshopping.shopfront.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import uk.co.danielbryant.djshopping.shopfront.services.ProductService;
 
